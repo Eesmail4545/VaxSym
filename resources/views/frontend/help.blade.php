d85e19e84b6253763d3fe83fa1ad04e20c5ce04e
Eesmail4545 <felewany03@gmail.com>
2023-07-29 01:47:32 +0300

    vaccination

---
+++ resources/views/frontend/help.blade.php
@@ -0,0 +1,46 @@
+<x-guest-layout>
+  <h1>Covid-19 FAQs</h1>
+  <p>Can't find the answer you are looking for? We've shared some of our most frequently asked questions to help you</p>
+  <div class="faq">
+    <h2>Is Covid-19 the same as common cold?</h2>
+    <p>No, Covid-19 is not the same as common cold. Covid-19 is caused by a different type of virus known as the novel coronavirus.</p>
+  </div>
+  <div class="faq">
+    <h2>Which Covid-19 vaccines has MOH approved for administration in Kenya?</h2>
+    <p>The MOH has approved several vaccines for administration in Kenya, including the Pfizer-BioNTech, AstraZeneca, Moderna and Johnson & Johnson vaccines.</p>
+  </div>
+  <div class="faq">
+    <h2>How many doses of Covid-19 vaccine do I need to take and at what intervals?</h2>
+    <p>The number of doses and the intervals between them depend on the specific vaccine. For example, the Pfizer-BioNTech and Moderna vaccines require two doses, while the Johnson & Johnson vaccine requires only one dose. The intervals between doses also vary, so it's important to follow the instructions given by your healthcare provider.</p>
+  </div>
+  <div class="faq">
+    <h2>What happens if I fail to take the second dose of Covid-19 Vaccine on time?</h2>
+    <p>If you fail to take the second dose of a vaccine on time, your immune response may not be as strong as it could be. It's important to follow the recommended schedule to ensure the best protection against Covid-19.</p>
+  </div>
+  <div class="faq">
+    <h2>Are the Covid-19 vaccines safe?</h2>
+    <p>Yes, the Covid-19 vaccines are safe. They have undergone rigorous testing and have been approved by regulatory authorities around the world.</p>
+  </div>
+  <div class="faq">
+    <h2>Who is eligible for Covid-19 vaccinations?</h2>
+    <p>Eligibility for Covid-19 vaccinations varies by country and region. In general, healthcare workers, the elderly, and those with underlying health conditions are given priority.</p>
+  </div>
+  <div class="faq">
+    <h2>Who is not eligible for Covid-19 Vaccination?</h2>
+    <p>People who have a history of severe allergic reactions to vaccines or vaccine components should not receive the Covid-19 vaccine. Additionally, the vaccine is not recommended for children under a certain age, depending on the vaccine.</p>
+  </div>
+  <div class="faq">
+    <h2>Does Covid-19 vaccines have any side effects?</h2>
+    <p>Yes, like all vaccines, the Covid-19 vaccines can have side effects. However, the side effects are usually mild and go away on their own within a few days.</p>
+  </div>
+  <div class="faq">
+    <h2>Are the Covid-19 Vaccines safe for people with chronic diseases and heart conditions?</h2>
+    <p>Yes, the Covid-19 vaccines are safe for people with chronic diseases and heart conditions. In fact, these individuals may be at higher risk for severe illness from Covid-19 and may benefit the most from vaccination.</p>
+  </div>
+  <div class="faq">
+    <h2>If I have already contracted Covid-19 disease and recovered, do I still need to get a Covid-19 vaccine?</h2>
+  </div>
+  <div class="faq">
+    <p>Yes, it is still recommended that you get vaccinated even if you have already had Covid-19 and recovered. This is because the immunity from natural infection may not be as strong or long-lasting as the immunity from vaccination.</p>
+  </div>
+</x-guest-layout>
